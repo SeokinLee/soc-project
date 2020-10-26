59cca963b7bdb20c9565bcbf835bf2046d22f34e
LeeSeokin <miner01@naver.com>
2020-10-26 21:34:41 +0900

    1026 update

--- src/main/java/com/seokin/book/springboot/web/IndexController.java
+++ src/main/java/com/seokin/book/springboot/web/IndexController.java
@@ -34,6 +34,14 @@ public class IndexController {
         }
         return "index3";
     }
+    @GetMapping("/en")
+    public String index_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "index3_en";
+    }
 
     //목록 페이지
     @GetMapping("/list")
@@ -44,6 +52,14 @@ public class IndexController {
         }
         return "list";
     }
+    @GetMapping("/list/en")
+    public String list_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "list_en";
+    }
 
     //최적경로 페이지
     @GetMapping("/path")
@@ -53,6 +69,14 @@ public class IndexController {
             model.addAttribute("userName", user.getName());
         }
         return "path";
+    }
+    @GetMapping("/path/en")
+    public String path_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "path_en";
     }
 
     //통계 페이지
@@ -68,6 +92,19 @@ public class IndexController {
         }
         return "statistics";
     }
+    @GetMapping("/statistics/en")
+    public String statistics_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        model.addAttribute("device1", device1Service.findAllDesc());
+        model.addAttribute("device2", device2Service.findAllDesc());
+        model.addAttribute("device3", device3Service.findAllDesc());
+        model.addAttribute("device4", device4Service.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "statistics_en";
+    }//여기까지함
+
     @GetMapping("/statistics/device1")
     public String device1(Model model, @LoginUser SessionUser user) {
         model.addAttribute("device1", device1Service.findAllDesc());
@@ -75,6 +112,14 @@ public class IndexController {
             model.addAttribute("userName", user.getName());
         }
         return "device1";
+    }
+    @GetMapping("/statistics/device1/en")
+    public String device1_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("device1", device1Service.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "device1_en";
     }
     @GetMapping("/statistics/device2")
     public String device2(Model model, @LoginUser SessionUser user) {
@@ -84,6 +129,14 @@ public class IndexController {
         }
         return "device2";
     }
+    @GetMapping("/statistics/device2/en")
+    public String device2_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("device2", device2Service.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "device2_en";
+    }
     @GetMapping("/statistics/device3")
     public String device3(Model model, @LoginUser SessionUser user) {
         model.addAttribute("device3", device3Service.findAllDesc());
@@ -91,6 +144,14 @@ public class IndexController {
             model.addAttribute("userName", user.getName());
         }
         return "device3";
+    }
+    @GetMapping("/statistics/device3/en")
+    public String device3_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("device3", device3Service.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "device3_en";
     }
     @GetMapping("/statistics/device4")
     public String device4(Model model, @LoginUser SessionUser user) {
@@ -100,7 +161,14 @@ public class IndexController {
         }
         return "device4";
     }
-
+    @GetMapping("/statistics/device4/en")
+    public String device4_en(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("device4", device4Service.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "device4_en";
+    }
 
     @GetMapping("/posts/save")
     public String postsSave() {
@@ -114,5 +182,19 @@ public class IndexController {
         model.addAttribute("post", dto);
 
         return "posts-update";
+    }
+
+    @GetMapping("/posts/save/en")
+    public String postsSave_en() {
+        return "posts-save_en";
+    }//post-save.mustache 호출
+
+    @GetMapping("/posts/update/{id}/en")
+    //Model : PostsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+    public String postsUpdate_en(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update_en";
     }
 }
